b95edee3519e2a4a0b1d11e3cc3c276ab790f0ad
Sam McGrath <mcg13004@byui.edu>
2018-01-11 10:57:19 -0700

    finished a couple functions

--- main.js
+++ main.js
@@ -5,24 +5,95 @@
 /* Put dependencies here */
 
 /* Include this line only if you are going to use Canvas API */
-// const canvas = require('canvas-wrapper');
+const canvas = require('canvas-wrapper');
+const asyncLib = require('async');
+
+/* Variables */
+
+//array of objects
+// id -- > quiz id
+// question --> quiz question text
+// answer --> quiz answer
+var quizId = [];
 
 /* View available course object functions */
 // https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
 module.exports = (course, stepCallback) => {
-    /* Create the module report so that we can access it later as needed.
-    This MUST be done at the beginning of each child module. */
-    course.addModuleReport('moduleName');
+    //Create the module report so that we can access it later as needed.
+    course.addModuleReport('match-question-answers');
 
-    /* Used to log successful actions */
-    course.success('moduleName', 'moduleName successfully ...');
+    //Get list of all quizzes -- canvas.getQuizzes
+    //iterate through quizzes and get all questions -- canvas.getQuizQuestions && identify all matching questions -- array of IDs
+    //iterate through all matching questions and swap questions and answers
 
-    /* How to report an error (Replace "moduleName") */
-    // course.throwErr('moduleName', e);
+    /*********************************************
+    * getQuizzes
+    * Retrieves a list of quizzes in a course and
+    * builds an object array based on each quiz
+    **********************************************/
+    function getQuizzes(course, functionCallback) {
+        canvas.getQuizzes(course.info.canvasOU, (getErr, quiz_list) => {
+            if (getErr) {
+                functionCallback(getErr);
+                return;
+            } else {
+                course.success(`match-question-answers`, `Successfully retrieved ${quiz_list.length} quizzes.`);
+                functionCallback(null, course, quiz_list);
+            }
+        }, (err) => {
+            if (err) {
+                functionCallback(err);
+                return;
+            }
+        });
+    }
 
-    /* You should never call the stepCallback with an error. We want the
-    whole program to run when testing so we can catch all existing errors */
+    function filterQuizQuestions(course, quiz_items, functionCallback) {
+        var questionType = [
+            'matching_question'
+        ];
 
-    stepCallback(null, course);
+        asyncLib.eachLimit(quiz_items, 3, (item, eachCallback) => {
+            canvas.getQuizQuestions(course.info.canvasOU, quiz_items, (getErr, questions) => {
+                if (getErr) {
+                    functionCallback(getErr);
+                    return;
+                } else {
+                    questions.forEach(question => {
+                        if (questionType.includes(question.question_type)) {
+                            var obj = {
+                                'id': question.id,
+                                'question': question.question_text,
+                                'answer': question.answer
+                            };
+
+                            quizId.push(obj);
+                        }
+                    });
+                }
+            });
+        }, (err) => {
+            if (err) {
+                functionCallback(err);
+            } else {
+                course.success(`match-question-answers`, `Successfully filtered the quiz questions`);
+            }
+        });
+    }
+
+    var functions = [
+        getQuizzes,
+        filterQuizQuestions
+    ];
+
+    asyncLib.waterfall(functions, (waterfallErr, results) => {
+        if (waterfallErr) {
+            course.throwErr(`match-question-answers`, waterfallErr);
+            stepCallback(null, course);
+        } else {
+            course.success(`match-questions-answers`, `Successfully completed match-questions-answers`);
+            stepCallback(null, course);
+        }
+    });
 };
